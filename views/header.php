1b1cca3a2943308f996b4570b47c203c562d860d
edvwib <edde275@gmail.com>
2017-12-07 14:54:34 +0100

    add normalize.css

--- views/header.php
+++ views/header.php
@@ -1,2 +1,22 @@
 <?php
 declare(strict_types=1);
+
+//Always load setup
+require __DIR__.'/../app/autoload.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title><?php echo $config['title'] ?></title>
+
+    <link rel="stylesheet" href="/../assets/css/normalize.css">
+    <link rel="stylesheet" href="/../assets/css/main.css">
+
+</head>
+<body>
+
+</body>
+</html>
