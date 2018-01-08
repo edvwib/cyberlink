1f6ed44f2ebcd03048f3835a20a8f7bfc095cf48
edvwib <edde275@gmail.com>
2018-01-08 22:03:27 +0100

    update display of postlist

--- views/pages/postList.php
+++ views/pages/postList.php
@@ -9,15 +9,19 @@ foreach ($posts as $post) {
     $user = getUser($post['user_id'], $pdo);
     ?>
     <div class="row">
-        <div class="col-8 offset-2 post">
+        <div class="col-1 offset-1 vote">
+
+        </div>
+        <div class="col-8 post">
             <span class="col-1 h4 score"><?php echo ($score===1)?($score.'pt'):($score.'pts') ?></span>
-            <span class="col-11 h2"><a href="<?php echo $post['link'] ?>"><?php echo $post['title']; ?></a></span>
+            <a class="col-11 h3" href="<?php echo $post['link'] ?>"><?php echo $post['title']; ?></a>
             <a class="h6" href="http://<?php echo parse_url($post['link'], PHP_URL_HOST); ?>">(<?php echo parse_url($post['link'], PHP_URL_HOST); ?>)</a>
-            <blockquote class="blockquote">
-                <p class="mb-0"><?php echo $post['description'] ?></p>
-                <a href="?page=user&user=<?php echo $user ?>" class="blockquote-footer">/u/<?php echo $user ?></a>
+            <blockquote class="col-12 mb-0 blockquote">
+                <p class="mb-0"><?php echo ($post['description']!=null)?(substr($post['description'], 0, 105). '...'):('No description to display') ?></p>
             </blockquote>
-            <h5 class="col-2"><a href="?page=post&post=<?php echo $post['post_id'] ?>">comments</a></h5>
+            <a class="col-2" href="?page=user&user=<?php echo $user ?>">/u/<?php echo $user ?></a>
+            <?php echo $spacer ?>
+            <a class="col-2" href="?page=post&post=<?php echo $post['post_id'] ?>">comments</a>
         </div>
     </div>
     <?php
