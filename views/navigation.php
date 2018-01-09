dcdecc6cd00b6e2da20c8b008bcd79ed0fbc2c49
edvwib <edde275@gmail.com>
2018-01-09 09:51:37 +0100

    update navigation

--- views/navigation.php
+++ views/navigation.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 ?>
 
-<ul class="nav flex-column flex-sm-row justify-content-center">
+<ul class="nav nav-pills flex-column flex-sm-row justify-content-center">
     <li class="nav-item">
         <a class="nav-link <?php if($query['page'] === 'start'){echo 'active';} ?>" href="/">Frontpage</a>
     </li>
@@ -10,11 +10,13 @@ declare(strict_types=1);
         <a class="nav-link <?php if($query['page'] === 'newpost'){echo 'active';} ?>" href="?page=newpost">New post</a>
     </li>
     <?php if ($_SESSION['authenticated']): ?>
-        <li class="nav-item">
-            <a class="nav-link <?php if($query['page'] === 'user'){echo 'active';} ?>" href="?page=user&user=<?php echo $_SESSION['user']['username'] ?>">You (<?php echo $_SESSION['user']['username'] ?>)</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="?page=logout">Logout</a>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle <?php if($query['page'] === 'user'){echo 'active';} ?>" href="?page=user&user=<?php echo $_SESSION['user']['username'] ?>" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">You (<?php echo $_SESSION['user']['username'] ?>)</a>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="?page=profile">Edit profile</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="?page=logout">Logout</a>
+            </div>
         </li>
     <?php else: ?>
         <li class="nav-item">
