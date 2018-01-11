e03e18f5aec1e0498dfb0cebc8d7e79bb9e00205
edvwib <edde275@gmail.com>
2018-01-11 20:27:45 +0100

    redirects user directly to new post when created

--- app/posts/newpost.php
+++ app/posts/newpost.php
@@ -3,23 +3,27 @@ declare(strict_types=1);
 
 require_once __DIR__.'/../../views/header.php';
 
-if (!empty($_POST['link']) && !empty($_POST['title'])) {
+if (!empty($_POST['link']) && !empty($_POST['title']))
+{
     $link = filter_var($_POST['link'], FILTER_SANITIZE_URL);
     $title = filter_var($_POST['title'], FILTER_SANITIZE_STRING);
     $description = filter_var($_POST['description'], FILTER_SANITIZE_STRING);
 
-    if(!filter_var($link, FILTER_VALIDATE_URL)){
-        $link = 'http://'.$link;
-        if (!filter_var($link, FILTER_VALIDATE_URL)) {
-            echo 'Not a valid link';
-        }
-    }
+    // if(!filter_var($link, FILTER_VALIDATE_URL))
+    // {
+    //     $link = 'http://'.$link;
+    //     if (!filter_var($link, FILTER_VALIDATE_URL))
+    //     {
+    //         echo 'Not a valid link';
+    //     }
+    // }
 
     $addPost = $pdo->prepare("INSERT INTO posts
                             (link, title, time, user_id, description)
                             VALUES
-                            (:link, :title, date('now'), :user_id, :description)");
-    if(!$addPost){
+                            (:link, :title, strftime('%s'), :user_id, :description)");
+    if(!$addPost)
+    {
         die(var_dump($pdo->errorInfo()));
     }
     $addPost->bindParam(':link', $link, PDO::PARAM_STR);
@@ -28,5 +32,15 @@ if (!empty($_POST['link']) && !empty($_POST['title'])) {
     $addPost->bindParam(':description', $description, PDO::PARAM_STR);
     $addPost->execute();
 
-    redirect('/');
+    $linkToPost = $pdo->prepare("SELECT post_id FROM posts WHERE user_id=:user_id ORDER BY 'time' desc LIMIT 1");
+    $linkToPost->bindParam(':user_id', $_SESSION['user']['user_id'], PDO::PARAM_INT);
+    $linkToPost->execute();
+    $linkToPost = $linkToPost->fetch();
+
+    redirect('/?page=post&post='.$linkToPost['post_id']);
+}
+else
+{
+    $_SESSION['forms']['invalidPost'] = true;
+    redirect('/?page=newpost');
 }
--- views/pages/newPost.php
+++ views/pages/newPost.php
@@ -18,10 +18,14 @@ declare(strict_types=1);
             <div class="form-group">
                 <textarea class="col-6 form-control" type="text" name="description" placeholder="Description..." rows="3"></textarea>
             </div>
+            <?php if (isset($_SESSION['forms']['invalidPost']) && $_SESSION['forms']['invalidPost']): ?>
+                <p class="col-6 bg-danger text-white">Please enter at least a link and a title.</p>
+                <?php $_SESSION['forms']['invalidPost'] = false; ?>
+            <?php endif; ?>
             <button class="col-6 btn" type="submit" name="submit">Post</button>
         </form>
     <?php else: ?>
-        <p>You need to be <a href="?page=login">logged in</a> in order to create posts.</p>
+        <p class="col-6 bg-warning text-white">You need to be <a href="?page=login">logged in</a> in order to create posts.</p>
     <?php endif; ?>
   </div>
 </div>
